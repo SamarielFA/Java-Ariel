d14f0b19ed86836ecead3c9e7e76b096987edc32
SamarielFA <165931736+SamarielFA@users.noreply.github.com>
2024-04-03 19:19:03 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+        int temp = 45;
+
+        if (temp >= 0 && temp <=5) {
+          System.out.println("Wear a warm jacket!");
+        }
+        else if (temp >= 15 && temp <=30) {
+          System.out.println("Wear a T-shirt!");
+        }
+        else if (temp >= 30 && temp <=40) {
+          System.out.println("Wear a swimming suite!");
+        }
+        else {
+          System.out.println("You need cooling!");
+        }
+
+
+      }
+    }
